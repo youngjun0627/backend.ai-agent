6e827a3c79bddee79f95ef9290e6b66b16333894
Joongi Kim <joongi@lablup.com>
2015-10-25 02:51:00 +0900

    refs #3: Implement docker controller (need to test)

--- sorna/agent/server.py
+++ sorna/agent/server.py
@@ -3,16 +3,21 @@
 import asyncio, zmq, aiozmq
 import argparse
 import docker
-import logging
+import logging, logging.config
 import aiobotocore
 from namedlist import namedtuple
 import signal
+import sys
 from sorna.proto import Message, odict, generate_uuid
 from sorna.proto.msgtypes import *
 
-log = logging.getLogger(__name__)
+log = logging.getLogger('sorna.agent.server')
+log.setLevel(logging.DEBUG)
 container_registry = dict()
-
+container_ports_available = set(p for p in range(2001, 2100))
+volume_root = '/var/lib/sorna-volumes'
+docker_addr = 'tcp://127.0.0.1:2375'
+supported_langs = frozenset(['python27', 'python34'])
 
 @asyncio.coroutine
 def heartbeat(loop, agent_addr, manager_addr):
@@ -23,41 +28,25 @@ def heartbeat(loop, agent_addr, manager_addr):
     '''
     global container_registry
     while True:
-        # TODO: replace explicit manager request with direct Redis calls
-        msg = Message()
-        msg['req_type'] = ManagerRequestTypes.HEARTBEAT
-        msg['agent_addr'] = agent_addr
         # TODO: attach the list of currently running kernels
         # TODO: add extra info (e.g., capacity available)
-        master_sock = yield from aiozmq.create_zmq_stream(zmq.REQ,
-                connect=manager_addr, loop=loop)
-        master_sock.write([msg.encode()])
-        try:
-            _ = yield from asyncio.wait_for(master_sock.read(), timeout=1, loop=loop)
-        except asyncio.TimeoutError:
-            log.error('Master does not respond to heartbeat.')
-            # TODO: terminate myself if master does not respond for a long time.
-        finally:
-            master_sock.close()
-        yield from asyncio.sleep(3)
+        yield from asyncio.sleep(3, loop=loop)
 
 @asyncio.coroutine
-def run_agent(loop, server_sock, manager_addr):
+def run_agent(loop, server_sock, manager_addr, agent_addr):
     global container_registry
 
     # Resolve the master address
     if manager_addr is None:
-        # TODO: implement
-        manager_addr = 'tcp://{0}:{1}'
+        manager_addr = 'tcp://sorna-manager.lablup:5001'
 
     # Initialize docker subsystem
-    # TODO: implement
-    # TODO: if docker is already running, init from existing container list?
+    cli = docker.Client(docker_addr, timeout=1)
     container_registry.clear()
 
     # Send the first heartbeat.
     asyncio.async(heartbeat(loop, agent_addr, manager_addr), loop=loop)
-    yield from asyncio.sleep(0)
+    yield from asyncio.sleep(0, loop=loop)
 
     # Then start running the agent loop.
     while True:
@@ -71,26 +60,36 @@ def run_agent(loop, server_sock, manager_addr):
         if request['req_type'] == AgentRequestTypes.CREATE_KERNEL:
 
             log.info('CREATE_KERNEL ({})'.format(request['lang']))
-            if request['lang'] == 'python34':
-                resp['reply'] = SornaResponseTypes.SUCCESS
+            if request['lang'] in supported_langs:
+
                 kernel_id = generate_uuid()
-                # TODO: create a container with Python 3.4 image
-                # TODO: add to container_registry
-                # TODO: return the kernel ID
+                kernel_port = container_ports_available.pop()
+                work_dir = os.path.join(volume_root, kernel_id)
+                os.makedirs(work_dir)
+                container_id = cli.create_container('kernel-{}'.format(request['lang']),
+                                                    mem_limit='128m',
+                                                    memswap_limit=0,
+                                                    cpu_shares=1024, # full share
+                                                    ports=[2001],
+                                                    host_config=docker.utils.create_host_config(
+                                                       port_bindings={2001: ('127.0.0.1', kernel_port)},
+                                                       binds={
+                                                           '/home/work': {'bind': work_dir, 'mode': 'rw'},
+                                                       }),
+                                                    tty=False)
+                container_info = cli.insepct_container(container_id)
+                kernel_ip = container_info['NetworkSettings']['IPAddress']
                 container_registry[kernel_id] = {
-                    'addr': '...',
                     'lang': request['lang'],
+                    'container_id': container_id,
+                    'addr': 'tcp://{0}:{1}'.format(kernel_ip, kernel_port),
+                    'ip': kernel_ip,
+                    'port': kernel_port,
                 }
-            elif request['lang'] == 'python27':
+                # TODO: (asynchronously) check if container is running okay.
                 resp['reply'] = SornaResponseTypes.SUCCESS
-                kernel_id = generate_uuid()
-                # TODO: create a container with Python 2.7 image
-                # TODO: add to container_registry
-                # TODO: return the kernel ID
-                container_registry[kernel_id] = {
-                    'addr': '...',
-                    'lang': request['lang'],
-                }
+                resp['kernel_id'] = kernel_id
+
             else:
                 resp['reply'] = SornaResponseTypes.INVALID_INPUT
                 resp['body'] = 'Unsupported kernel language.'
@@ -98,14 +97,29 @@ def run_agent(loop, server_sock, manager_addr):
         elif request['req_type'] == AgentRequestTypes.DESTROY_KERNEL:
 
             log.info('DESTROY_KERNEL ({})'.format(request['kernel_id']))
-            # TODO: destroy the container
-            # TODO: remove from container_registry
+            if request['kernel_id'] in container_registry:
+
+                kernel_id = request['kernel_id']
+                kernel_port = container_registry[kernel_id]['port']
+                container_id = container_registry[kernel_id]['container_id']
+                # TODO: use graceful shutdown using aiodocker?
+                cli.kill(container_id)
+                cli.remove_container(container_id)
+                work_dir = os.path.join(volume_root, kernel_id)
+                shutil.rmtree(work_dir)
+                container_ports_available.add(kernel_port)
+                del container_registry[kernel_id]
+
+            else:
+                resp['reply'] = SornaResponseTypes.INVALID_INPUT
+                resp['body'] = 'No such kernel.'
 
         elif request['req_type'] == AgentRequestTypes.EXECUTE:
 
             log.info('EXECUTE')
             try:
-                container_addr = container_registry[request['kernel_id']]['addr']
+                kernel_id = request['kernel_id']
+                container_addr = container_registry[kernel_id]['addr']
                 #redirect_output = request.get('redirect_output', False)
             except KeyError:
                 resp['reply'] = SornaResponseTypes.INVALID_INPUT
@@ -119,7 +133,8 @@ def run_agent(loop, server_sock, manager_addr):
 
                 # Execute with a 4 second timeout.
                 try:
-                    result_data = yield from asyncio.wait_for(container_sock.read(),
+                    read_task = asyncio.Task(container_sock.read())
+                    result_data = yield from asyncio.wait_for(read_task,
                                                               timeout=4, loop=loop)
 
                     # TODO: check updated files in container /home/work volume.
@@ -127,13 +142,15 @@ def run_agent(loop, server_sock, manager_addr):
                     result = json.loads(result_data[0])
                     resp['reply'] = SornaResponseTypes.SUCCESS
                     resp['body'] = odict(
-                        ('eval_result': result['eval_result']),
-                        ('stdout': result['stdout']),
-                        ('stderr': result['stderr']),
-                        ('exceptions': result['exceptions']),
-                        ('files': []),
+                        ('eval_result', result['eval_result']),
+                        ('stdout', result['stdout']),
+                        ('stderr', result['stderr']),
+                        ('exceptions', result['exceptions']),
+                        ('files', []),
                     )
+                    # TODO: handle connection error
                 except asyncio.TimeoutError:
+                    read_task.cancel()
                     resp['reply'] = SornaResponseTypes.FAILURE
                     resp['body'] = 'TimeoutError'
                 finally:
@@ -146,7 +163,6 @@ def run_agent(loop, server_sock, manager_addr):
 
 
 def main():
-
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--agent-port', type=int, default=6001)
     argparser.add_argument('--manager-addr', type=str, default=None)
@@ -159,6 +175,7 @@ def main():
             'console': {
                 'class': 'logging.StreamHandler',
                 'level': 'DEBUG',
+                'stream': 'ext://sys.stdout',
             },
             'null': {
                 'class': 'logging.NullHandler',
@@ -179,25 +196,19 @@ def main():
         },
     })
     agent_addr = 'tcp://*:{0}'.format(args.agent_port)
-
-    def handle_exit():
-        raise SystemExit()
-
-    asyncio.set_event_loop_policy(aiozmq.ZmqEventLoopPolicy())
     loop = asyncio.get_event_loop()
     server_sock = loop.run_until_complete(aiozmq.create_zmq_stream(zmq.REP, bind=agent_addr, loop=loop))
-    loop.add_signal_handler(signal.SIGTERM, handle_exit)
-
-    log.info('serving at {1}'.format(agent_addr))
+    log.info('serving at {0}'.format(agent_addr))
     try:
-        asyncio.async(run_agent(loop, server_sock, args.manager_addr), loop=loop)
+        asyncio.async(run_agent(loop, server_sock, args.manager_addr, agent_addr), loop=loop)
         loop.run_forever()
     except (KeyboardInterrupt, SystemExit):
-        server.close()
+        server_sock.close()
         for t in asyncio.Task.all_tasks():
-            t.cancel()
+            if not t.done():
+                t.cancel()
         try:
-            loop._run_once()
+            loop.run_until_complete(asyncio.sleep(0, loop=loop))
         except asyncio.CancelledError:
             pass
     finally:
