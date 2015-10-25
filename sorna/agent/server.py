3057c321465c11630486522f0952ba58f70f1ca4
Joongi Kim <joongi@lablup.com>
2015-10-25 15:16:56 +0900

    refs #3: Restructure agent service loop.

--- sorna/agent/server.py
+++ sorna/agent/server.py
@@ -33,6 +33,82 @@ def heartbeat(loop, agent_addr, manager_addr):
         yield from asyncio.sleep(3, loop=loop)
 
 @asyncio.coroutine
+def create_kernel(loop):
+    kernel_id = generate_uuid()
+    assert kernel_id not in container_registry
+    kernel_port = container_ports_available.pop()
+    work_dir = os.path.join(volume_root, kernel_id)
+    os.makedirs(work_dir)
+    container_id = cli.create_container('kernel-{}'.format(request['lang']),
+                                        mem_limit='128m',
+                                        memswap_limit=0,
+                                        cpu_shares=1024, # full share
+                                        ports=[2001],
+                                        host_config=docker.utils.create_host_config(
+                                           port_bindings={2001: ('127.0.0.1', kernel_port)},
+                                           binds={
+                                               '/home/work': {'bind': work_dir, 'mode': 'rw'},
+                                           }),
+                                        tty=False)
+    container_info = cli.inspect_container(container_id)
+    kernel_ip = container_info['NetworkSettings']['IPAddress']
+    container_registry[kernel_id] = {
+        'lang': request['lang'],
+        'container_id': container_id,
+        'addr': 'tcp://{0}:{1}'.format(kernel_ip, kernel_port),
+        'ip': kernel_ip,
+        'port': kernel_port,
+    }
+    return kernel_id
+
+@asyncio.coroutine
+def destroy_kernel(loop, kernel_id):
+    global container_registry
+    kernel_port = container_registry[kernel_id]['port']
+    container_id = container_registry[kernel_id]['container_id']
+    cli.kill(container_id)  # forcibly shut-down the container
+    cli.remove_container(container_id)
+    work_dir = os.path.join(volume_root, kernel_id)
+    shutil.rmtree(work_dir)
+    container_ports_available.add(kernel_port)
+    del container_registry[kernel_id]
+
+@asyncio.coroutine
+def execute_code(loop, kernel_id, cell_id, code):
+    # TODO: read the file list in container /home/work volume
+    container_id = container_registry[kernel_id]['container_id']
+    work_dir = os.path.join(volume_root, container_id)
+    #os.listdir(work_dir)
+
+    container_addr = container_registry[kernel_id]['addr']
+    container_sock = yield from aiozmq.create_zmq_stream(zmq.REQ,
+            connect=container_addr, loop=loop)
+    container_sock.write([cell_id, code])
+
+    # Execute with a 4 second timeout.
+    try:
+        result_data = yield from asyncio.wait_for(container_sock.read(),
+                                                  timeout=4, loop=loop)
+        # TODO: check updated files in container /home/work volume.
+        # TODO: upload updated files to s3
+        result = json.loads(result_data[0])
+        return odict(
+            ('eval_result', result['eval_result']),
+            ('stdout', result['stdout']),
+            ('stderr', result['stderr']),
+            ('exceptions', result['exceptions']),
+            ('files', []),
+        )
+    except asyncio.TimeoutError as exc:
+        log.warning('Timeout detected on kernel {} (cell_id: {}).'
+                    .format(kernel_id, cell_id))
+        yield from destroy_kernel(loop, kernel_id)
+        resp['reply'] = SornaResponseTypes.FAILURE
+        resp['body'] = type(exc).__name__
+    finally:
+        container_sock.close()
+
+@asyncio.coroutine
 def run_agent(loop, server_sock, manager_addr, agent_addr):
     global container_registry
 
@@ -61,35 +137,16 @@ def run_agent(loop, server_sock, manager_addr, agent_addr):
 
             log.info('CREATE_KERNEL ({})'.format(request['lang']))
             if request['lang'] in supported_langs:
-
-                kernel_id = generate_uuid()
-                kernel_port = container_ports_available.pop()
-                work_dir = os.path.join(volume_root, kernel_id)
-                os.makedirs(work_dir)
-                container_id = cli.create_container('kernel-{}'.format(request['lang']),
-                                                    mem_limit='128m',
-                                                    memswap_limit=0,
-                                                    cpu_shares=1024, # full share
-                                                    ports=[2001],
-                                                    host_config=docker.utils.create_host_config(
-                                                       port_bindings={2001: ('127.0.0.1', kernel_port)},
-                                                       binds={
-                                                           '/home/work': {'bind': work_dir, 'mode': 'rw'},
-                                                       }),
-                                                    tty=False)
-                container_info = cli.inspect_container(container_id)
-                kernel_ip = container_info['NetworkSettings']['IPAddress']
-                container_registry[kernel_id] = {
-                    'lang': request['lang'],
-                    'container_id': container_id,
-                    'addr': 'tcp://{0}:{1}'.format(kernel_ip, kernel_port),
-                    'ip': kernel_ip,
-                    'port': kernel_port,
-                }
-                # TODO: (asynchronously) check if container is running okay.
-                resp['reply'] = SornaResponseTypes.SUCCESS
-                resp['kernel_id'] = kernel_id
-
+                try:
+                    kernel_id = yield from create_kernel(loop)
+                    # TODO: (asynchronously) check if container is running okay.
+                except Exception as exc:
+                    resp['reply'] = SornaResponseTypes.FAILURE
+                    resp['body'] = type(exc).__name__
+                    log.exception(exc)
+                else:
+                    resp['reply'] = SornaResponseTypes.SUCCESS
+                    resp['kernel_id'] = kernel_id
             else:
                 resp['reply'] = SornaResponseTypes.INVALID_INPUT
                 resp['body'] = 'Unsupported kernel language.'
@@ -98,63 +155,37 @@ def run_agent(loop, server_sock, manager_addr, agent_addr):
 
             log.info('DESTROY_KERNEL ({})'.format(request['kernel_id']))
             if request['kernel_id'] in container_registry:
-
-                kernel_id = request['kernel_id']
-                kernel_port = container_registry[kernel_id]['port']
-                container_id = container_registry[kernel_id]['container_id']
-                # TODO: use graceful shutdown using aiodocker?
-                cli.kill(container_id)
-                cli.remove_container(container_id)
-                work_dir = os.path.join(volume_root, kernel_id)
-                shutil.rmtree(work_dir)
-                container_ports_available.add(kernel_port)
-                del container_registry[kernel_id]
-
+                try:
+                    yield from destroy_kernel(loop, request['kernel_id'])
+                except Exception as exc:
+                    resp['reply'] = SornaResponseTypes.FAILURE
+                    resp['body'] = type(exc).__name__
+                    log.exception(exc)
+                else:
+                    resp['reply'] = SornaResponseTypes.SUCCESS
             else:
                 resp['reply'] = SornaResponseTypes.INVALID_INPUT
                 resp['body'] = 'No such kernel.'
 
         elif request['req_type'] == AgentRequestTypes.EXECUTE:
 
-            log.info('EXECUTE')
-            try:
-                kernel_id = request['kernel_id']
-                container_addr = container_registry[kernel_id]['addr']
-                #redirect_output = request.get('redirect_output', False)
-            except KeyError:
+            log.info('EXECUTE ({}, {})'.format(request['kernel_id'],
+                                               request['cell_id']))
+            if request['kernel_id'] in container_registry:
+                try:
+                    result = yield from execute_code(loop, kernel_id,
+                                                     request['cell_id'],
+                                                     request['code'])
+                except Exception as exc:
+                    resp['reply'] = SornaResponseTypes.FAILURE
+                    resp['body'] = type(exc).__name__
+                    log.exception(exc)
+                else:
+                    reps['reply'] = SornaResponseTypes.SUCCESS
+                    resp['body'] = result
+            else:
                 resp['reply'] = SornaResponseTypes.INVALID_INPUT
                 resp['body'] = 'Could not find such kernel.'
-            else:
-                # TODO: read the file list in container /home/work volume
-
-                container_sock = yield from aiozmq.create_zmq_stream(zmq.REQ,
-                        connect=container_addr, loop=loop)
-                container_sock.write([request['cell_id'], request['code']])
-
-                # Execute with a 4 second timeout.
-                try:
-                    read_task = asyncio.Task(container_sock.read())
-                    result_data = yield from asyncio.wait_for(read_task,
-                                                              timeout=4, loop=loop)
-
-                    # TODO: check updated files in container /home/work volume.
-                    # TODO: upload updated files to s3
-                    result = json.loads(result_data[0])
-                    resp['reply'] = SornaResponseTypes.SUCCESS
-                    resp['body'] = odict(
-                        ('eval_result', result['eval_result']),
-                        ('stdout', result['stdout']),
-                        ('stderr', result['stderr']),
-                        ('exceptions', result['exceptions']),
-                        ('files', []),
-                    )
-                    # TODO: handle connection error
-                except asyncio.TimeoutError:
-                    read_task.cancel()
-                    resp['reply'] = SornaResponseTypes.FAILURE
-                    resp['body'] = 'TimeoutError'
-                finally:
-                    container_sock.close()
         else:
             assert False, 'Invalid kernel request type.'
 
