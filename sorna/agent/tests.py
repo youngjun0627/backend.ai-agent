8d933b4218f565161310a7aa252a3019dc7f3611
Joongi Kim <joongi@lablup.com>
2015-11-01 14:44:38 +0900

    Add a test that complicates production deployment...
     * And it works as expected in test environment.... :(

--- sorna/agent/tests.py
+++ sorna/agent/tests.py
@@ -97,7 +97,6 @@ with open('large.txt', 'wb') as f:
 
     def test_restricted_networking(self):
         kernel_id = self.loop.run_until_complete(create_kernel(self.loop, self.docker_cli, 'python34'))
-        work_dir = os.path.join(volume_root, kernel_id)
         src = '''
 import socket
 socket.setdefaulttimeout(1.0)
@@ -108,11 +107,51 @@ try:
     s.close()
 except OSError:
     print('failed')
-'''.format(max_upload_size + 1)
+'''
         result = self.loop.run_until_complete(
                 execute_code(self.loop, self.docker_cli, 'test', kernel_id, '1', src))
         assert 'failed' in result['stdout']
         self.loop.run_until_complete(destroy_kernel(self.loop, self.docker_cli, kernel_id))
+
+    def test_heavy_code(self):
+        kernel_id = self.loop.run_until_complete(create_kernel(self.loop, self.docker_cli, 'python34'))
+        src = '''
+# alphametics.py
+import re
+import itertools
+
+def solve(puzzle):
+    words = re.findall('[A-Z]+', puzzle.upper())
+    unique_characters = set(''.join(words))
+    assert len(unique_characters) <= 10, 'Too many letters'
+    first_letters = {word[0] for word in words}
+    n = len(first_letters)
+    sorted_characters = ''.join(first_letters) + \
+        ''.join(unique_characters - first_letters)
+    characters = tuple(ord(c) for c in sorted_characters)
+    digits = tuple(ord(c) for c in '0123456789')
+    zero = digits[0]
+    for guess in itertools.permutations(digits, len(characters)):
+        if zero not in guess[:n]:
+            equation = puzzle.translate(dict(zip(characters, guess)))
+            if eval(equation):
+                return equation
+
+if __name__ == '__main__':
+    puzzles = ["HAWAII + IDAHO + IOWA + OHIO == STATES"]
+    # puzzles = ["I + LOVE + YOU == DORA"]  # 주석을 제거해서 다른 식으로도 해보세요
+    # puzzles = ["SEND + MORE == MONEY"]
+    for puzzle in puzzles:
+        print(puzzle)
+        solution = solve(puzzle)
+        if solution:
+            print(solution)
+'''
+        with self.assertRaises(asyncio.TimeoutError):
+            result = self.loop.run_until_complete(
+                    execute_code(self.loop, self.docker_cli, 'test', kernel_id, '1', src))
+        # it should have been automaitcally deleted
+        assert kernel_id not in container_registry
 
 '''
 class AgentKernelResponseTest(unittest.TestCase):
