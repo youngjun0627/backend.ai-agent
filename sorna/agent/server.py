a60c9ab176422fb6f3ea257aaabf56a82943beb1
Joongi Kim <joongi@lablup.com>
2016-11-11 17:41:16 +0900

    Increase the idle timeout to 30 minutes for tutorial/demo sessions.

--- sorna/agent/server.py
+++ sorna/agent/server.py
@@ -402,7 +402,7 @@ async def cleanup_timer(loop, docker_cli):
         keys = tuple(container_registry.keys())
         for kern_id in keys:
             try:
-                if now - container_registry[kern_id]['last_used'] > 60.0:
+                if now - container_registry[kern_id]['last_used'] > 1800.0:
                     log.info('destroying kernel {} as clean-up'.format(kern_id))
                     await destroy_kernel(loop, docker_cli, kern_id)
             except KeyError:
