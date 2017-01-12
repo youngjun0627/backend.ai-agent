4aa7bc3e6456d2f960d9db9a64a5442dc4866031
Jonghyun Park <adrysn@gmail.com>
2017-01-12 12:01:05 +0900

    Add unit tests for server.py (WIP)

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,9 +1,11 @@
+import asyncio
+
 from unittest import mock
 
 import asynctest
 import pytest
 
-
+from sorna.agent.resources import CPUAllocMap
 from sorna.agent.server import (
     get_extra_volumes, heartbeat_timer, stats_timer, cleanup_timer,
     match_result, AgentRPCServer
@@ -95,3 +97,123 @@ def test_match_result():
     assert not match_result(mock_result, mock_match)
     mock_result['stdout'] = 'mock_value'
     assert match_result(mock_result, mock_match)
+
+
+class TestAgentRPCServer:
+    @pytest.fixture
+    def mock_agent(self):
+        mock_docker = mock.Mock()
+        mock_args = mock.Mock()
+        mock_events = mock.Mock()
+
+        agent = AgentRPCServer(mock_docker, mock_args, mock_events)
+
+        return agent
+
+    def test_initialization(self):
+        mock_docker = mock.Mock()
+        mock_args = mock.Mock()
+        mock_events = mock.Mock()
+
+        agent = AgentRPCServer(mock_docker, mock_args, mock_events)
+
+        assert isinstance(agent.loop, asyncio.BaseEventLoop)
+        assert agent.docker == mock_docker
+        assert agent.config == mock_args
+        assert agent.events == mock_events
+        assert agent.container_registry == {}
+        assert isinstance(agent.container_cpu_map, CPUAllocMap)
+
+    def test_ping(self, mock_agent):
+        msg = mock_agent.ping('ping~')
+        assert msg == 'ping~'
+
+    @pytest.mark.asyncio
+    async def test_create_kernel(self, mock_agent):
+        agent = mock_agent
+        agent._create_kernel = asynctest.CoroutineMock(return_value='fakeid')
+        agent.container_registry['fakeid'] = {'stdin_port': 1,
+                                              'stdout_port': 2}
+
+        id, inport, outport = await agent.create_kernel('python3', {})
+
+        agent._create_kernel.assert_called_once_with('python3')
+        assert id == 'fakeid'
+        assert inport == 1
+        assert outport == 2
+
+    @pytest.mark.asyncio
+    async def test_destroy_kernel(self, mock_agent):
+        agent = mock_agent
+        agent._destroy_kernel = asynctest.CoroutineMock()
+        await agent.destroy_kernel('fakeid')
+        agent._destroy_kernel.assert_called_once_with('fakeid',
+                                                      'user-requested')
+
+    @pytest.mark.asyncio
+    async def test_restart_kernel(self, mock_agent):
+        agent = mock_agent
+        agent.container_registry['fakeid'] = {'lang': 'python3',
+                                              'stdin_port': 1,
+                                              'stdout_port': 2}
+        agent._create_kernel = asynctest.CoroutineMock(return_value='fakeid')
+        agent._destroy_kernel = asynctest.CoroutineMock()
+
+        inport, outport = await agent.restart_kernel('fakeid')
+
+        agent._create_kernel.assert_called_once_with('python3',
+                                                     kernel_id='fakeid')
+        agent._destroy_kernel.assert_called_once_with('fakeid', 'restarting')
+        assert inport == 1
+        assert outport == 2
+
+    @pytest.mark.asyncio
+    async def test_execute_code(self, mocker, mock_agent):
+        agent = mock_agent
+        agent._execute_code = asynctest.CoroutineMock(return_value={'mock': 1})
+        mock_match_result = mocker.patch('sorna.agent.server.match_result',
+                                         return_value=True)
+
+        result = await agent.execute_code(
+            entry_id='fakeentry', kernel_id='fakekernel', code_id='codeid',
+            code='print(1)', match=None)
+
+        agent._execute_code.assert_called_once_with('fakeentry', 'fakekernel',
+                                                    'codeid', 'print(1)')
+        mock_match_result.assert_not_called()
+        assert result == {'mock': 1}
+
+    @pytest.mark.asyncio
+    async def test_execute_code_match_result(self, mocker, mock_agent):
+        agent = mock_agent
+        agent._execute_code = asynctest.CoroutineMock(return_value={'mock': 1})
+        mock_match_result = mocker.patch('sorna.agent.server.match_result',
+                                         return_value=True)
+
+        match = {
+            'op': 'contains',
+            'target': 'stdout',
+            'value': 'mock_value',
+        }
+        await agent.execute_code(
+            entry_id='fakeentry', kernel_id='fakekernel', code_id='codeid',
+            code='print(1)', match=match)
+
+        mock_match_result.assert_called()
+
+    @pytest.mark.skip('not implemented yet')
+    def test_reset(self):
+        pass
+
+    @pytest.mark.skip('not implemented yet')
+    def test__create_kernel(self):
+        pass
+
+    @pytest.mark.skip('not implemented yet')
+    def test__destroy_kernel(self):
+        pass
+
+    @pytest.mark.skip('not implemented yet')
+    def test__execute_kernel(self):
+        pass
+
