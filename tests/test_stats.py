f4144c540b2f13e34b30d078ec1e5cf9c6683ce5
Jonghyun Park <adrysn@gmail.com>
2017-12-27 18:07:03 +0900

    test: remove mock tests for stats.py

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,108 +1,93 @@
-from pathlib import Path
-import sys
-from unittest import mock
+import asyncio
 
-import asynctest
+import aiodocker
 import pytest
 
-from ai.backend.agent.stats import (
-    _collect_stats_sysfs, _collect_stats_api, collect_stats, read_sysfs
-)
+from ai.backend.agent import stats
+
+
+@pytest.fixture(scope='session')
+def prepare_docker_images():
+    event_loop = asyncio.get_event_loop()
+    async def pull():
+        docker = aiodocker.Docker()
+        images_to_pull = [
+            'alpine:latest',
+        ]
+        for img in images_to_pull:
+            try:
+                await docker.images.get(img)
+            except aiodocker.exceptions.DockerError as e:
+                assert e.status == 404
+                print(f'Pulling image "{img}" for testing...')
+                await docker.pull(img)
+        await docker.close()
+    event_loop.run_until_complete(pull())
 
 
 @pytest.fixture
-def mock_container():
-    mock_container = mock.Mock()
-    mock_container._id = 'fakeid'
-    mock_container.stats = asynctest.CoroutineMock()
-    mock_container.stats.return_value = {
-        'cpu_stats': {
-            'cpu_usage': {
-                'total_usage': 1e5,
-            }
-        },
-        'memory_stats': {
-            'max_usage': 1000,
-        },
-        'blkio_stats': {
-            'io_service_bytes_recursive': [
-                {
-                    'op': 'Read',
-                    'value': 10,
-                },
-                {
-                    'op': 'Write',
-                    'value': 20,
-                },
-            ]
-        },
-        'networks': {
-            '_': {
-                'rx_bytes': 30,
-                'tx_bytes': 40,
-            },
-            '+': {
-                'rx_bytes': 50,
-                'tx_bytes': 60,
-            },
-        }
+def docker(event_loop, prepare_docker_images):
+    docker = aiodocker.Docker()
+    yield docker
+
+    async def finalize():
+        await docker.close()
+    event_loop.run_until_complete(finalize())
+
+
+@pytest.fixture
+def container(event_loop, docker):
+    container = None
+    config = {
+        'Cmd': ['-c', 'echo hello'],
+        'Entrypoint': 'sh',
+        'Image': 'alpine:latest',
     }
 
-    return mock_container
+    async def spawn():
+        nonlocal container
+        container = await docker.containers.create_or_replace(
+            config=config,
+            name='test-container'
+        )
+        await container.start()
+    event_loop.run_until_complete(spawn())
 
+    yield container
 
-@pytest.mark.skipif(sys.platform != 'linux',
-                    reason="sysfs is only available in Linux")
-def test_collect_stats_sysfs(mocker, mock_container):
-    mock_read_sysfs = mocker.patch('ai.backend.agent.stats.read_sysfs')
-    mock_read_sysfs.side_effect = [1e5, 1024, 16]
-
-    stat = _collect_stats_sysfs(mock_container)
-
-    assert stat['cpu_used'] == 1e5 / 1e6
-    assert stat['mem_max_bytes'] == 1024
-
-
-@pytest.mark.asyncio
-async def test_collect_stats_api(mock_container):
-    stat = await _collect_stats_api(mock_container)
-
-    assert stat['cpu_used'] == 1e5 / 1e6
-    assert stat['mem_max_bytes'] == 1000
-    assert stat['net_rx_bytes'] == 80
-    assert stat['net_tx_bytes'] == 100
-    assert stat['io_read_bytes'] == 10
-    assert stat['io_write_bytes'] == 20
+    async def finalize():
+        nonlocal container
+        if container:
+            await container.delete(force=True)
+    event_loop.run_until_complete(finalize())
 
 
 @pytest.mark.asyncio
-async def test_collect_stats(mocker):
-    mock_sys = mocker.patch('ai.backend.agent.stats.sys')
-    mock_sysfs = mocker.patch('ai.backend.agent.stats._collect_stats_sysfs')
-    mock_api = mocker.patch('ai.backend.agent.stats._collect_stats_api')
+async def test_collect_stats(container):
+    ret = await stats.collect_stats([container])
 
-    mock_sysfs.assert_not_called()
-    mock_api.assert_not_called()
-
-    # On linux platform, _collect_stats_sysfs should be called.
-    mock_sys.platform = 'linux'
-    await collect_stats([mock.Mock()])
-
-    mock_sysfs.assert_called()
-    sysfs_call_count = mock_sysfs.call_count
-    mock_api.assert_not_called()
-
-    # Else, _collect_stats_api should be called.
-    mock_sys.platform = '!linux'
-    with pytest.raises(TypeError):
-        await collect_stats([mock.Mock()])
-
-    assert mock_sysfs.call_count == sysfs_call_count
+    assert 'cpu_used' in ret[0]
+    assert 'mem_max_bytes' in ret[0]
+    assert 'mem_cur_bytes' in ret[0]
+    assert 'net_rx_bytes' in ret[0]
+    assert 'net_tx_bytes' in ret[0]
+    assert 'io_read_bytes' in ret[0]
+    assert 'io_write_bytes' in ret[0]
+    assert 'io_max_scratch_size' in ret[0]
+    assert 'io_cur_scratch_size' in ret[0]
 
 
-def test_read_sysfs(mocker):
-    mocker.patch.object(Path, 'read_text', return_value='2')
-    sysfs = read_sysfs('mock/path')
+def test_numeric_list():
+    s = '1 3 5 7'
+    ret = stats.numeric_list(s)
 
-    assert isinstance(sysfs, int)
-    assert sysfs == 2
+    assert ret == [1, 3, 5, 7]
+
+
+def test_read_sysfs(tmpdir):
+    p = tmpdir.join('test.txt')
+    p.write('1357')
+    ret = stats.read_sysfs(p)
+
+    assert isinstance(ret, int)
+    assert ret == 1357
