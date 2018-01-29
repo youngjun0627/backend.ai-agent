22821a9ef9b5fac49c0fd41ba26af868a9e059a4
Joongi Kim <joongi@lablup.com>
2018-01-30 01:02:40 +0900

    Rename get_src_version() to read_src_version() in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import re
 
 
-def get_src_version():
+def read_src_version():
     p = (Path(__file__).parent / 'ai' / 'backend' / 'agent' / '__init__.py')
     src = p.read_text()
     m = re.search(r"^__version__\s*=\s*'([^']+)'", src, re.M)
@@ -56,7 +56,7 @@ monitor_requires = [
 
 setup(
     name='backend.ai-agent',
-    version=get_src_version(),
+    version=read_src_version(),
     description='Backend.AI Agent',
     long_description=Path('README.rst').read_text(),
     url='https://github.com/lablup/backend.ai-agent',
