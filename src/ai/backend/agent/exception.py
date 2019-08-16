98d74517188ca2c60afd5d8947407c918556a1e5
Kyujin Cho <kyujin.cho@lablup.com>
2019-08-16 11:23:17 +0900

    Add new Exception definition for K8s

--- src/ai/backend/agent/exception.py
+++ src/ai/backend/agent/exception.py
@@ -4,3 +4,16 @@ class InitializationError(Exception):
 
 class InsufficientResource(Exception):
     pass
+
+class NoKernelError(Exception):
+    pass
+
+class K8sError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+class ScaleFailedError(Exception):
+    pass
+
+        
