86eb15818659dbe0ca43330df59b556004318cba
Kyujin Cho <kyujin.cho@lablup.com>
2019-08-21 12:35:50 +0900

    Remove unused exceptions

--- src/ai/backend/agent/exception.py
+++ src/ai/backend/agent/exception.py
@@ -6,15 +6,7 @@ class InsufficientResource(Exception):
     pass
 
 
-class NoKernelError(Exception):
-    pass
-
-
 class K8sError(Exception):
     def __init__(self, message):
         super().__init__(message)
         self.message = message
-
-
-class ScaleFailedError(Exception):
-    pass
