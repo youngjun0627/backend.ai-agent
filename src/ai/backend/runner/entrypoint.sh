e968b497246a76be6991e5e5e5f1e5861d57bed0
Joongi Kim <joongi@lablup.com>
2019-10-14 12:04:59 +0900

    entrypoint: Fix errors when group already exists

--- src/ai/backend/runner/entrypoint.sh
+++ src/ai/backend/runner/entrypoint.sh
@@ -25,13 +25,14 @@ else
 
   USER_NAME=work
   echo "Setting up user/group: $USER_NAME ($USER_ID:$GROUP_ID)"
-  if [ -f /bin/ash ]; then  # for alpine
+  if [ "$(getent group $GROUP_ID)" -ne 0 ]; then
     addgroup -g $GROUP_ID $USER_NAME
-    adduser -s /bin/ash -h "/home/$USER_NAME" -H -D -u $USER_ID -G $USER_NAME -g "User" $USER_NAME
+  fi
+  if [ -f /bin/ash ]; then  # for alpine
+    adduser -s /bin/ash -h "/home/$USER_NAME" -H -D -u $USER_ID -G $GROUP_ID -g "User" $USER_NAME
     export SHELL=/bin/ash
   else
-    groupadd -g $GROUP_ID $USER_NAME
-    useradd -s /bin/bash -d "/home/$USER_NAME" -M -r -u $USER_ID -g $USER_NAME -o -c "User" $USER_NAME
+    useradd -s /bin/bash -d "/home/$USER_NAME" -M -r -u $USER_ID -g $GROUP_ID -o -c "User" $USER_NAME
     export SHELL=/bin/bash
   fi
   export LD_LIBRARY_PATH="/opt/backend.ai/lib:$LD_LIBRARY_PATH"
@@ -43,9 +44,9 @@ else
   fi
 
   # Correct the ownership of agent socket.
-  chown work:work /opt/kernel/agent.sock
+  chown $USER_ID:$GROUP_ID /opt/kernel/agent.sock
 
   echo "Executing the main program..."
-  exec /opt/kernel/su-exec $USER_NAME:$USER_NAME "$@"
+  exec /opt/kernel/su-exec $USER_ID:$GROUP_ID "$@"
 
 fi
